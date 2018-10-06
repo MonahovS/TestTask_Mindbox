4007a6a8aa04c88060943b1bcf9b8f2fe9af10c0
Сергей Монахов <cemm302@yandex.ru>
2018-10-06 13:36:58 +0300

    Добавьте файлы проекта.

---
+++ SquareSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SquareSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
